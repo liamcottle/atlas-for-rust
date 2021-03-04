ead6d743b21b901a1f4dc968cc7bd03461edf49b
liamcottle <liam@liamcottle.com>
2021-03-05 05:29:59 +1300

    remove focus ring

--- src/components/ConnectSteamAccount.vue
+++ src/components/ConnectSteamAccount.vue
@@ -17,7 +17,7 @@
             </div>
           </div>
 
-          <button @click="connectWithSteam" type="button" class="w-full inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          <button @click="connectWithSteam" type="button" class="w-full inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none">
             <span class="mx-auto">Connect with Steam</span>
           </button>
 
