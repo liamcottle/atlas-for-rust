93c8c35571379637ed39ed1732cfd2c804afaaaf
liamcottle <liam@liamcottle.com>
2021-03-05 19:06:15 +1300

    add custom named z indexes

--- src/components/modals/AddServerModal.vue
+++ src/components/modals/AddServerModal.vue
@@ -1,6 +1,6 @@
 <template>
   <Transition name="fade">
-    <div v-if="isShowing" class="fixed z-10 inset-0 overflow-y-auto">
+    <div v-if="isShowing" class="fixed z-modal inset-0 overflow-y-auto">
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
         <div @click="close" class="fixed inset-0 transition-opacity" aria-hidden="true">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    zIndex: {
+      'modal': 1000,
+      'tooltip': 1001,
+      'auto': 'auto',
+    }
   },
   variants: {
     extend: {
