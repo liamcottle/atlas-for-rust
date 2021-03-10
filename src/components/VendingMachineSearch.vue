f5ed01bd772b8e4aa356206f6b6b73965c2c089f
liamcottle <liam@liamcottle.com>
2021-03-11 03:38:21 +1300

    remove marker icon

--- src/components/VendingMachineSearch.vue
+++ src/components/VendingMachineSearch.vue
@@ -28,11 +28,6 @@
         <div v-for="vendingMachine in vendingMachinesWithSearchedItems" class="mb-4">
 
           <div @click="showVendingMachine(vendingMachine)" class="flex bg-gray-700 rounded-t p-2 cursor-pointer">
-            <div class="flex-none my-auto mr-2">
-              <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-              </svg>
-            </div>
             <div class="flex-grow my-auto text-sm">{{vendingMachine.name}}</div>
             <div class="flex-none my-auto ml-2">
               <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
